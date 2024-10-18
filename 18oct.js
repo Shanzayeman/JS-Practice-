899d0481020039062cf7c6ed8cc2138e0898473a
ShanzayEman <shanzayeman0@gmail.com>
2024-10-18 20:56:19 +0500

    Add files via upload

---
+++ 18oct.js
@@ -0,0 +1,13 @@
+document.getElementById("demo1").innerHTML = Math.abs(-4.7);
+document.getElementById("demo2").innerHTML = 
+"The sine value of 90 degrees is " + Math.sin(90 * Math.PI / 180);
+document.getElementById("demo3").innerHTML = 
+"The cosine value of 0 degrees is " + Math.cos(0 * Math.PI / 180);
+document.getElementById("demo4").innerHTML =
+Math.min(0, 150, 30, 20, -8, -200);
+document.getElementById("demo5").innerHTML =
+Math.max(0, 150, 30, 20, -8, -200);
+document.getElementById("demo6").innerHTML = Math.random();
+document.getElementById("demo7").innerHTML = Math.log(2);
+document.getElementById("demo8").innerHTML = Math.log2(8);
+document.getElementById("demo9").innerHTML = Math.log10(1000);
